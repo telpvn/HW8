c5ebb10df58e63805797b1aa5c6f291c930d1585
telpvn <telpvn@mail.ru>
2024-04-27 18:13:19 +0400

    2 task

--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //todo код программы писать тут
+        double[] weights = new double[10];
+        double middleWeight = 0;
+        int peopleCount = 0;
+
+        for (int i = 0; i < weights.length; i++) {
+            weights[i] = (new Random().nextDouble() * 60) + 40;
+            middleWeight += weights[i];
+            if (weights[i] >= 60 && weights[i] <= 80) {
+                peopleCount++;
+            }
+
+
+        }
+        System.out.println("Средний вес равен: " + (middleWeight / weights.length));
+        System.out.println("Кол-во людей: " + peopleCount);
     }
 }
